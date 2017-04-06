cafe0c12b62ed5f932d88892c7d53d75e3030bc7
Reuben <reuben.coutinho@gmail.com>
2017-04-06 21:29:47 +1000

    Chore: added basic resume template

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-var fs = require('fs');
-var resumeSchema = require('resume-schema');
-var resumeObject = JSON.parse(fs.readFileSync('resume.json', 'utf8'));
-resumeSchema.validate(resumeObject, (report, err) => {
-    if (err) {
-        console.log("Error occured: "+err.message)
-        return;
-    }
-    if (report.valid)
-        console.log("Report is valid")
-    else
-        console.log("Report is not valid")
-});
+var fs = require("fs");
+var path = require('path');
+var Handlebars = require("handlebars");
+
+function render(resume) {
+	var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
+	var tpl = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
+	var partialsDir = path.join(__dirname, 'partials');
+	var filenames = fs.readdirSync(partialsDir);
+
+	filenames.forEach(function (filename) {
+	  var matches = /^([^.]+).hbs$/.exec(filename);
+	  if (!matches) {
+	    return;
+	  }
+	  var name = matches[1];
+	  var filepath = path.join(partialsDir, filename)
+	  var template = fs.readFileSync(filepath, 'utf8');
+
+	  Handlebars.registerPartial(name, template);
+	});
+	return Handlebars.compile(tpl)({
+		css: css,
+		resume: resume
+	});
+}
+
+module.exports = {
+	render: render
+};
