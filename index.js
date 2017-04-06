19872b95ead5e93c13c183711f1dc50b8afe453b
Reuben <reuben.coutinho@gmail.com>
2017-04-06 21:02:01 +1000

    Refactor: updated code to validate resume

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 var fs = require('fs');
-var resumeSchema  = require('resume-schema');
+var resumeSchema = require('resume-schema');
 var resumeObject = JSON.parse(fs.readFileSync('resume.json', 'utf8'));
-resumeSchema.validate(resumeObject);
+resumeSchema.validate(resumeObject, (report, err) => {
+    if (err) {
+        console.log("Error occured: "+err.message)
+        return;
+    }
+    if (report.valid)
+        console.log("Report is valid")
+    else
+        console.log("Report is not valid")
+});
